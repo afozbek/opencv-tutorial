718433c866a7efbefc040502b861b7062a30fae1
Furkan Ozbek <fozbek@boynergrup.com>
2019-12-06 15:08:39 +0300

    datalar eklendi

---
+++ 15-21/tutorial-16.py
@@ -0,0 +1,11 @@
+import cv2                       # BGR
+import numpy as np
+
+"""Making your own Haar Cascade"""
+
+# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
